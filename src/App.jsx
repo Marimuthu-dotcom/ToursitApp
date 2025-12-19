7c8f0f18e0588ea117be25662efceedec1926c2f
Marimuthu <maripavin7@gmail.com>
2025-12-19 19:30:03 +0530

    Create a Logo Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import Home from "./pages/Home"
-function App(){
-    return(
-        <>
-        <div>
-            <Home />
-        </div>
-        </>
-    );
+import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import LogoPage from "./components/LogoPage";
+
+function App() {
+  const [showHome, setShowHome] = useState(false);
+
+  useEffect(() => {
+    const hasSeenLogo = sessionStorage.getItem("hasSeenLogo");
+
+    if (hasSeenLogo) {
+      setShowHome(true);
+    }
+  }, []);
+
+  const handleLogoFinish = () => {
+    sessionStorage.setItem("hasSeenLogo", "true"); // mark as seen
+    setShowHome(true);
+  };
+
+  return <>
+  {showHome ? <Home /> : <LogoPage onFinish={handleLogoFinish} />}
+  </>;
 }
-export default App; 
+
+export default App;
